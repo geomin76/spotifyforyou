de44ba39c7b76d90318625993bb3dbcb8976ac4e
geomin76 <geom@vt.edu>
2020-07-30 19:15:27 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,186 @@
+from flask import Flask
+from os import abort
+import sys
+import os
+
+from flask import render_template, request, redirect, session, url_for
+from six.moves.urllib.parse import quote
+import json
+from spotify_service import *
+
+app = Flask(__name__)
+
+
+# CLIENT_ID = secrets.client_id()
+# CLIENT_SECRET = secrets.client_secret()
+# app.secret_key = secrets.secret_key()
+
+CLIENT_ID = os.environ.get('ID')
+CLIENT_SECRET = os.environ.get('SECRET')
+app.secret_key = os.environ.get('SECRET_KEY')
+
+
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_URL = "https://api.spotify.com/v1"
+
+# CLIENT_SIDE_URL = "https://playlist-statistics-267218.appspot.com/"
+# REDIRECT_URI = 'https://playlist-statistics-267218.appspot.com/callback'
+CLIENT_SIDE_URL = "http://spotifyforyou.com"
+REDIRECT_URI = 'http://spotifyforyou.com/callback'
+
+#for testing locally
+# CLIENT_SIDE_URL='http://127.0.0.1:8080'
+# REDIRECT_URI = 'http://127.0.0.1:8080/callback'
+
+SCOPE = "user-library-read user-read-currently-playing playlist-read-collaborative user-library-modify playlist-read-private playlist-modify-public playlist-modify-private user-top-read user-modify-playback-state user-read-playback-state"
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    "client_id": CLIENT_ID
+}
+
+#directs to introduction page
+@app.route('/')
+def main():
+    return render_template('intro.html')
+
+
+@app.route("/authorize")
+def authorize():
+    # Auth Step 1: Authorization
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+
+#after getting user access token, it redirects to home page
+@app.route('/callback')
+def callback():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+    response_data = json.loads(post_request.text)
+    session['tokens'] = {
+        'access_token': response_data["access_token"],
+        'refresh_token': response_data["refresh_token"]
+    }
+    return redirect(url_for('index'))
+
+
+@app.route('/index')
+def index():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    sp = spotipy.Spotify(auth=session['tokens'].get('access_token'))
+    data = user_content(sp)
+    playlists = playlist(sp)
+    return render_template('index.html', data=data, playlists=playlists)
+
+
+@app.route('/playlist_intro')
+def playlist_intro():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    sp = spotipy.Spotify(auth=session['tokens'].get('access_token'))
+    playlists = playlist(sp)
+    return render_template('playlist_intro.html', playlists=playlists)
+
+
+@app.route('/playlist_select', methods=['GET', 'POST'])
+def playlist_select():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    select = request.form.get('comp_select')
+    sp = spotipy.Spotify(auth=session['tokens'].get('access_token'))
+    stats = playlist_calculation(sp, select)
+    playlist_name = (select.split("'"))[3]
+    return render_template("playlist_select.html", playlist=playlist_name, stats=stats)
+
+
+@app.route("/recs")
+def recs():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    sp = spotipy.Spotify(auth=session['tokens'].get('access_token'))
+    genres = []
+    for gen in sp.recommendation_genre_seeds()['genres']:
+        genres.append(gen)
+    return render_template("recs.html", genre=genres)
+
+
+@app.route("/recs_final", methods=['GET', 'POST'])
+def recs_final():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    specs = {
+        'name': request.form['name'],
+        'number': request.form['quantity'],
+        'select': request.form.getlist('comp_select'),
+        'dance': request.form['danceability'],
+        'energy': request.form['energy'],
+        'valence': request.form['valence']
+    }
+    if not specs.get('dance'):
+        specs.pop('dance')
+    if not specs.get('energy'):
+        specs.pop('energy')
+    if not specs.get('valence'):
+        specs.pop('valence')
+    name = recs_playlist(session['tokens'].get('access_token'), specs=specs)
+    return render_template("recs_final.html", name=name)
+
+
+@app.route("/top_stats_intro")
+def top_stats_intro():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    return render_template("top_stats_intro.html")
+
+
+@app.route("/top_stats", methods=['GET', 'POST'])
+def top_stats():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    limit = request.form['limit']
+    time = request.form['time']
+    top = get_top(session['tokens'].get('access_token'), limit, time)
+    return render_template("top_stats.html", stats=top)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/related_artists")
+def related_artists():
+    return render_template("related_artists.html")
+
+@app.route("/for_nerds")
+def for_nerds():
+    if 'tokens' not in session:
+        return render_template("intro.html")
+    sp = spotipy.Spotify(auth=session['tokens'].get('access_token'))
+    data = user_content(sp)
+    return render_template("for_nerds.html")
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+@app.errorhandler(401)
+def spotify_exception(error):
+    return render_template('401.html'), 401
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ spotify_service.py
@@ -0,0 +1,138 @@
+import requests
+import spotipy
+
+
+def user_content(sp):
+    results = sp.current_user_playing_track()
+    if results:
+        link = results['item']['external_urls']['spotify']
+        parsing = link.split('/')
+        newlink = parsing[0] + '//' + parsing[1] + '/' + parsing[2] + "/embed/" + parsing[3] + '/' + parsing[4]
+        stats = sp.audio_features(parsing[4])
+        complete_stats = {
+            "dance": int(stats[0]['danceability'] * 100),
+            "valence": int(stats[0]['valence'] * 100),
+            "energy": int(stats[0]['energy'] * 100),
+            "tempo": int(stats[0]['tempo'])
+        }
+        user_data = sp.current_user()['display_name']
+        temp = {
+            "name": results['item']['name'],
+            "artist": results['item']['artists'][0]['name'],
+            "link": newlink,
+            "stats": complete_stats,
+            "user": user_data
+        }
+        return temp
+
+#This function gets the user's playlist
+def playlist(sp):
+    playlists = sp.current_user_playlists()
+    list = []
+    for playlist in playlists['items']:
+        # if playlist['owner']['id'] == sp.current_user()['id']:
+        id = playlist['external_urls']['spotify'].split("/")[4]
+        playlist_obj = {
+            "name": playlist['name'],
+            "id": id
+        }
+        list.append(playlist_obj)
+    return list
+
+def playlist_calculation(sp, select):
+    count = 0
+    danceability = 0
+    valence = 0
+    energy = 0
+    tempo = 0
+    song_list = []
+    # spotify's audio features for songs only limits to 50, this breaks that and allows up to 1000 songs i think
+    for song in sp.playlist_tracks(select.split("'")[7])['items']:
+        audio_feature = sp.audio_features(song['track']['external_urls']['spotify'].split("/")[4])
+        song_list.append(song['track']['external_urls']['spotify'].split("/")[4])
+        danceability += audio_feature[0]['danceability']
+        valence += audio_feature[0]['valence']
+        energy += audio_feature[0]['energy']
+        tempo += audio_feature[0]['tempo']
+        count += 1
+    avg_danceability = int((danceability * 100) / count)
+    avg_valence = int((valence * 100) / count)
+    avg_energy = int((energy * 100) / count)
+    avg_tempo = int(tempo / count)
+    stats = {
+        'danceability': avg_danceability,
+        'valence': avg_valence,
+        'energy': avg_energy,
+        'tempo': avg_tempo
+    }
+    return stats
+
+def recs_playlist(token, specs):
+    url = "https://api.spotify.com/v1/recommendations?"
+    url += "limit=" + str(specs['number']) + "&market=US&"
+    url += "seed_genres="
+    #fix this, specs['select'] is list now!
+    count = len(specs['select'])
+    if count == 1:
+        url += str((specs['select'])[0])
+    else:
+        i = 0
+        while i != count - 1:
+            url += str((specs['select'])[i]) + "%2C"
+            i+=1
+        url += str((specs['select'])[count - 1])
+    if 'dance' in specs:
+        dance = float(specs['dance']) / 100
+        url += "&target_danceability=" + str(dance)
+    if 'energy' in specs:
+        energy = float(specs['energy']) / 100
+        url += "&target_energy=" + str(energy)
+    if 'valence' in specs:
+        valence = float(specs['valence']) / 100
+        url += "&target_valence=" + str(valence)
+    #add popularity and other specs
+    tracks = []
+    data = requests.get(url, headers={"Authorization": 'Bearer ' + token}).json()
+    for track in data['tracks']:
+        tracks.append(track['uri'])
+    sp = spotipy.Spotify(auth=token)
+    user_id = sp.current_user()['id']
+    sp.user_playlist_create(user_id, specs['name'], public=True,
+                            description="Playlist created by Spotify For You")
+    playlist_id = (sp.current_user_playlists(1, 0)['items'][0]['id'])
+    sp.user_playlist_add_tracks(user_id, playlist_id, tracks)
+    return specs['name']
+
+
+def get_top(token, limit, time):
+    limit = str(limit)
+    url = "https://api.spotify.com/v1/me/top/artists?time_range=" + time + "&limit=" + limit
+    artist_data = requests.get(url, headers={"Authorization": 'Bearer ' + token}).json()
+    url = "https://api.spotify.com/v1/me/top/tracks?time_range=" + time + "&limit=" + limit
+    track_data = requests.get(url, headers={"Authorization": 'Bearer ' + token}).json()
+    artists = []
+    for artist in artist_data['items']:
+        # print(artist['images'])
+        artists.append(artist['name'])
+    tracks = []
+    for track in track_data['items']:
+        tracks.append(track['name'])
+    complete_obj = {
+        'artists': artists,
+        'tracks': tracks
+    }
+    return complete_obj
+
+def for_nerds(sp):
+    results = sp.current_user_playing_track()
+    if results:
+        link = results['item']['external_urls']['spotify']
+        parsing = link.split('/')
+        newlink = parsing[0] + '//' + parsing[1] + '/' + parsing[2] + "/embed/" + parsing[3] + '/' + parsing[4]
+        stats = sp.audio_features(parsing[4])
+    complete_stats = {
+
+    }
+    return complete_stats
+
+
